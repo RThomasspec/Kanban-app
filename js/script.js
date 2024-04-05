66a56ba7b099affc0708a21ce47b8315b607e6bf
ThomasAidesNsoft <thomasbages@outlook.fr>
2024-04-05 18:03:49 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+function addTask(idContainer, idInput){
+    const inputBox = document.getElementById(idInput);
+    if(inputBox.value === "" || inputBox.value.trim() === ""){
+        // message error
+    }else{
+        const listcontainer = document.getElementById(idContainer);
+        let barreHorizontal = document.createElement("div");
+
+        barreHorizontal.classList.add("barre-horizontal");
+        listcontainer.appendChild(barreHorizontal);
+        let li = document.createElement("li");
+        //innerHtml sert a denir le contenu html de la balise précisé
+        li.innerHTML = inputBox.value;
+        // permet d'ajouter un composant html au composant précisé
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);   
+    }
+    inputBox.value="";
+
+}
+
+const listContainers = document.querySelectorAll('.list-container');
+listContainers.forEach(container => {
+    
+    container.addEventListener('click', function(e) {
+        if(e.target.tagName === "SPAN"){
+            const liElement = e.target.parentNode;
+            liElement.remove();
+        }
+    }, false);
+
+});
+
+

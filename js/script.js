9af8cd08e54a169def6a0a520ff8326f40e31c14
ThomasAidesNsoft <thomasbages@outlook.fr>
2024-04-08 15:05:04 +0200

    ajout fonctionnalité drag and drop

--- js/script.js
+++ js/script.js
@@ -1,34 +1,72 @@
 
 
-function addTask(idContainer, idInput){
+const listContainers = document.querySelectorAll('.list-container');
+
+
+
+function addTask(idContainer, idInput) {
     const inputBox = document.getElementById(idInput);
-    if(inputBox.value === "" || inputBox.value.trim() === ""){
+    if (inputBox.value === "" || inputBox.value.trim() === "") {
         // message error
-    }else{
+    } else {
         const listcontainer = document.getElementById(idContainer);
         let barreHorizontal = document.createElement("div");
 
         barreHorizontal.classList.add("barre-horizontal");
         listcontainer.appendChild(barreHorizontal);
         let li = document.createElement("li");
+        li.classList.add("list");
         //innerHtml sert a denir le contenu html de la balise précisé
         li.innerHTML = inputBox.value;
+        li.setAttribute('draggable', 'true');
         // permet d'ajouter un composant html au composant précisé
         listcontainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
-        li.appendChild(span);   
+        li.appendChild(span);
+
+        let div = document.createElement('div');
+        div.classList.add("list-img");
+        let img = document.createElement('img');
+        img.src = '../img/grab.png';
+        img.setAttribute('draggable', 'false');
+
+        div.appendChild(img);
+        li.appendChild(div);
+
     }
-    inputBox.value="";
+    inputBox.value = "";
 
-} 
-
-
-const listContainers = document.querySelectorAll('.list-container');
-listContainers.forEach(container => {
+    const lists = document.querySelectorAll('.list');
+    for (const list of lists) {
+        //nous obtenons une référence à l'élément que l'utilisateur a glissé
+        list.addEventListener("dragstart", function (e) {
+            selected = e.target;
+            for (const listContainer of listContainers) {
     
-    container.addEventListener('click', function(e) {
-        if(e.target.tagName === "SPAN"){
+                // gestionnaire d'événements du conteneur cible
+                listContainer.addEventListener("dragover", function (e) {
+                    // permet au container de recevoir le drop
+                    e.preventDefault();
+                })
+                //  nous gérons le déplacement de l'élément déplaçable du conteneur d'origine vers la zone de dépôt
+                listContainer.addEventListener("drop", function (e) {
+                    if (selected) {
+                        listContainer.appendChild(selected);
+                        selected = null;
+                    }
+                })
+            }
+        })
+    }
+}
+
+
+const lists = document.querySelectorAll('.list');
+listContainers.forEach(container => {
+
+    container.addEventListener('click', function (e) {
+        if (e.target.tagName === "SPAN") {
             // tu viens recuperer le parent du span qui est li pour pouvoir le supprimer.
             const liElement = e.target.parentNode;
             liElement.remove();
@@ -36,5 +74,14 @@ listContainers.forEach(container => {
     }, false);
 
 });
+
+
+
+
+
+
+
+
+
 
 
